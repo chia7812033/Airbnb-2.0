9be7891a9fdeebfc4a9c35ef9d6a97997b740306
Dan Hung <dan891221@gmail.com>
2023-03-05 15:49:43 +0800

    Add progress bar when page changes

---
+++ pages/_app.jsx
@@ -0,0 +1,15 @@
+import "../styles/globals.css";
+
+import NextNProgress from "nextjs-progressbar";
+import Router from "next/router";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <NextNProgress options={{ easing: "ease", speed: 500 }} />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
